09a0b0b81d3a988996bfabed162f97f76112bf19
g.andrade <g.andrade@salu.com.vc>
2023-04-13 20:47:18 -0300

    fix

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, session, render_template, request, redirect
-from auth import SendResetPassword
+from flask import Flask, session, render_template, request, redirect, make_response
+from auth import SendResetPassword, CreateUserFirebase, SignFirebase
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/reset', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         email = request.form.get('email')
@@ -15,10 +15,40 @@ def index():
     return render_template('reset-password.html')
 
 
-@app.route('/logout')
-def logout():
-    pass
+@app.route('/', methods=['POST','GET'])
+def create():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+        if senha == "":
+            return "Senha invalida"
+        if email == "":
+            return "Email invalido"
+        
+        user = CreateUserFirebase(email, senha)
+        print(f"User ===> {user}")
+        if user['idToken'] != "":
+            return make_response(redirect('/login'))
 
+               
+    
+    return render_template('cadastro.html')
+
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+        if senha == "":
+            return "Senha invalida"
+        if email == "":
+            return "Email invalido"
+        try:
+            user = SignFirebase(email, senha)
+        except:
+            return make_response(redirect(login), error=user['error']['message'])
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
--- auth.py
+++ auth.py
@@ -1,12 +1,23 @@
 
 import pyrebase
-
+import requests
 
 
 
 
 
 def SendResetPassword(email):
+
+    config = {
+    'apiKey': "AIzaSyDMAChNn_jeQKdiqwPQWOLNPiB3GX_PAtM",
+    'authDomain': "ibooks-1fa58.firebaseapp.com",
+    'projectId': "ibooks-1fa58",
+    'storageBucket': "ibooks-1fa58.appspot.com",
+    'messagingSenderId': "516281803999",
+    'appId': "1:516281803999:web:31b0d833009469f4c1b56a",
+    'measurementId': "G-V8JNJ11FDL",
+    'databaseURL': ""
+    }
 
     firebase = pyrebase.initialize_app(config)
     auth = firebase.auth()
@@ -15,7 +26,69 @@ def SendResetPassword(email):
     return user
 
 
+def CreateUserFirebase(email, senha):
+
+    config = {
+    'apiKey': "AIzaSyDMAChNn_jeQKdiqwPQWOLNPiB3GX_PAtM",
+    'authDomain': "ibooks-1fa58.firebaseapp.com",
+    'projectId': "ibooks-1fa58",
+    'storageBucket': "ibooks-1fa58.appspot.com",
+    'messagingSenderId': "516281803999",
+    'appId': "1:516281803999:web:31b0d833009469f4c1b56a",
+    'measurementId': "G-V8JNJ11FDL",
+    'databaseURL': ""
+    }
+
+    firebase = pyrebase.initialize_app(config)
+    auth = firebase.auth()
+
+    try:
+        user = auth.create_user_with_email_and_password(email, senha)
+        return user
+    except requests.exceptions.HTTPError as e:
+        error_json = e.response
+        error_message = error_json['error']['message']
+        if error_message == "EMAIL_EXISTS":
+            print("O e-mail informado já está em uso.")
+        else:
+            print("Ocorreu um erro ao criar o usuário:", error_message)
+    except Exception as e:
+        print("Ocorreu um erro ao criar o usuário:", e)
+
+
+    return user
 
 
 
-#print(user)
+def SignFirebase(email,senha):
+
+    config = {
+    'apiKey': "AIzaSyDMAChNn_jeQKdiqwPQWOLNPiB3GX_PAtM",
+    'authDomain': "ibooks-1fa58.firebaseapp.com",
+    'projectId': "ibooks-1fa58",
+    'storageBucket': "ibooks-1fa58.appspot.com",
+    'messagingSenderId': "516281803999",
+    'appId': "1:516281803999:web:31b0d833009469f4c1b56a",
+    'measurementId': "G-V8JNJ11FDL",
+    'databaseURL': ""
+    }
+
+    firebase = pyrebase.initialize_app(config)
+    auth = firebase.auth()
+
+    try:
+        user = auth.sign_in_with_email_and_password(email, senha)
+    except requests.exceptions.HTTPError as e:
+        error_json = e.response.json()
+        error_message = error_json["error"]["message"]
+    except Exception as e:
+        print("Ocorreu um erro ao criar o usuário:", error_message)
+
+
+    return user
+
+
+
+
+
+
